60adcb55f0cfc91fe7504321902e4ed18321f592
ShauryaKharb <73641410+ShauryaKharb@users.noreply.github.com>
2023-01-31 06:10:08 +0530

    Update srjhvbsr.py

--- srjhvbsr.py
+++ srjhvbsr.py
@@ -5,20 +5,20 @@ def func1():
     a = str(input("Enter 1 or 2 : "))
     if a == "1" :
         state = "lost phone search"
-        print("you have entered", a, state)
+        ##print("you have entered", a,"i.e.", state)
         err1 = False
         proceed(state)
     elif a == "2":
         state = "buy new phone"
-        print("you have entered", a, state)
+        ##print("you have entered", a,"i.e.", state)
         err1 = False
-        proceed()
+        proceed(state)
     else :
         print("please enter only '1' or '2'")
         func1()
 #===============
 def proceed(x):
-    print(x,": STATEEEEEEEEE")
+    ##print(x,": STATEEEEEEEEE")
     print("Before going further we would need to know you.Please enter the details mentioned below.")
     
     if err2 == True :
@@ -29,14 +29,14 @@ def proceed(x):
         print("please enter a valid mobile number")
         mb_no = reproceed()
         #mb_no = reproceed()
-    print(x)
+    ##print(x)
     if x == "lost phone search" :
-        print(mb_no)
-        lostPhone()
+        ##print(mb_no)
+        lostPhone(name)
     else :
         #mb_no = reproceed()
-        print(mb_no)
-        newPhone()
+        ##print(mb_no)
+        newPhone(name)
        
 #===============
 def reproceed():
@@ -47,21 +47,22 @@ def reproceed():
         print("please enter a valid mobile number")
         reproceed()
     elif len(x) == 10 :
-        for i in x:
-            print(i)
-            if i == '0' or i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9' :
-                #return int(x)
-               wow +=1
-            else :
-                print("Please enter INTEGERS ONLY")
-                reproceed()
-        if wow == 10 :
-            return int(x)
+        #for i in x:
+        #    print(i)
+        #    if i == '0' or i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9' :
+        #        #return int(x)
+        #       wow +=1
+        #    else :
+        #        print("Please enter INTEGERS ONLY")
+        #        x = None
+        #        reproceed()
+        #if wow == 10 :
+        return int(x)
     else :
         return 0000000000
 #===============
-def lostPhone():
-    print("Lost Phone")
+def lostPhone(name):
+    ##print("Lost Phone")
     print("Select what you want to do next")
     print("1. regain info [type 1]")
     print("2. track location [type 2]")
@@ -73,16 +74,16 @@ def lostPhone():
     if y == 1 :
         print("The request has been sent, your info will be stored to your samsung account cloud")
     elif y == 2 :
-        print("Your phone was ast detected at Pathan chowk near Chuna Bhatti Road at 4:41 AM Jan 31st")
+        print(name , "your phone was ast detected at Pathan chowk near Chuna Bhatti Road at 4:41 AM Jan 31st")
     elif y == 3 :
-        print("Your phone will ring for 1 minute even if it is on DnD or Silent mode.")
+        print(name , "your phone will ring for 1 minute even if it is on DnD or Silent mode.")
     elif y == 4 :
-        print("Your device is currently not connected to a network, try again later")
+        print(name , "your device is currently not connected to a network, try again later")
     elif y == 5 :
-        print("Your device is now unlocked.")
+        print(name , "your device is now unlocked.")
 #===============
-def newPhone():
-    print("Sorry we are out of stock right now. We will soon be back with greater deals and discounts.")
+def newPhone(name):
+    print("Sorry", name,"we are out of stock right now. We will soon be back with greater deals and discounts.")
     
 #=============================================================================
 
@@ -98,4 +99,4 @@ name = None
 adres = None
 mb_no = None
 y = None
-func1() 
+func1()
